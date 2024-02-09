36512f33d112a32c98f0d8cf7987a910d9deac4e
brkim <brk0820@gmail.com>
2024-02-09 14:41:17 +0900

    0209 14:41

--- src/main/java/com/bookstore/member/dto/Role.java
+++ src/main/java/com/bookstore/member/dto/Role.java
@@ -1,14 +1,22 @@
 package com.bookstore.member.dto;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
 public enum Role {
 	USER("ROLE_USER", "member"),
 	ADMIN("ROLE_ADMIN", "admin");
 	
 	private final String key;
 	private final String title;
+	
+	private Role(String key, String title) {
+		this.key = key;
+		this.title = title;
+	}
+	
+	public String getKey() {
+		return key;
+	}
+	public String getTitle() {
+		return title;
+	}
+	
 }
